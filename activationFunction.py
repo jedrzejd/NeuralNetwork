36870a38efa2289e67d94179238a44af2df7dc9e
Jedrzej Dudzicz <jedrzejdudzicz@gmail.com>
2022-05-26 10:01:23 +0200

    git add NN

---
+++ activationFunction.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_prime(x):
+    return 1 - np.tanh(x) ** 2
+
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / y_true.size
---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from Layer import ActivationLayer, FCLayer
+from NeuralNetwork import Network
+from activationFunction import tanh, tanh_prime, mse, mse_prime
+
+x_train = np.array([[[0, 0]], [[0, 1]], [[1, 0]], [[1, 1]]])
+y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+
+# network
+net = Network()
+net.add(FCLayer(2, 3))
+net.add(ActivationLayer(tanh, tanh_prime))
+net.add(FCLayer(3, 1))
+net.add(ActivationLayer(tanh, tanh_prime))
+
+# train
+net.use(mse, mse_prime)
+net.fit(x_train, y_train, epochs=1000, learning_rate=0.1)
+
+# test
+out = net.predict(x_train)
+print(out)
